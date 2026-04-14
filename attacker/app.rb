0da28df26b8b9c8f8c99ff4823b25d7a9d737c05
Areia <aranzazu.mendez@fortra.com>
2026-04-14 12:14:56 -0600

    added notes

--- attacker/app.rb
+++ attacker/app.rb
@@ -3,10 +3,10 @@ require 'dotenv/load'
 require 'uri'
 
 proxy_url = URI::HTTP.build(
-    host: ENV['PROXY_HOST'],
-    port: ENV['PROXY_PORT'].to_i
-    userinfo: "#{ENV['PROXY_USER']}:#{ENV['PROXY_PASS']}"
-).to_s
+    host: ENV['PROXY_HOST'], # Obtiene el host del proxy
+    port: ENV['PROXY_PORT'].to_i # Obtiene el puerto del proxy y lo convierte a entero
+    userinfo: "#{ENV['PROXY_USER']}:#{ENV['PROXY_PASS']}" # Incluye credenciales de autenticación 
+).to_s # Convierte la URL del proxy a una cadena
 
 
 conn = Faraday.new(proxy: proxy_url) do |f|
